14c071738e62acba73a6cc3ea8b7a80b1566beac
preytor <pgrfpgm@gmail.com>
2023-08-17 12:28:56 -0400

    Added the footer to the project

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Pablo's Portfolio | Software Developer";
+  currentYear:number;
+  constructor(private _translateService: TranslateService) {
+    this._translateService.addLangs(['en', 'es']);
+    this._translateService.setDefaultLang('en');
+    this.currentYear=new Date().getFullYear();
+  }
+  
 }
