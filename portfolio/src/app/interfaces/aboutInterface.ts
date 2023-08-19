159e0359a2dc8b2e150b7289f47156947fc713df
preytor <pgrfpgm@gmail.com>
2023-08-19 11:19:46 -0400

    added the interfaces for about and footer

---
+++ portfolio/src/app/interfaces/aboutInterface.ts
@@ -0,0 +1,12 @@
+export interface aboutInterface {
+    title : string,
+    jobTitle  : string,
+    introductionOfMe  : string,
+    passionateAboutDevelopment: string,
+    mobility  : string,
+    years : string,
+    graduation: string,
+    researchDescriptio: string,
+    an: string,
+    followMeOn: string,
+}
---
+++ portfolio/src/app/interfaces/footerInterface.ts
@@ -0,0 +1,3 @@
+export interface footerInterface {
+    by: string
+}
