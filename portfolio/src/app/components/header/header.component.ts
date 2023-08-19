6fd0f4f2d88386d7221a9ae8b93edd0f6c2a7c89
preytor <pgrfpgm@gmail.com>
2023-08-19 09:44:30 -0400

    fixed the header translation to use the proper library

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -13,23 +13,14 @@ import { headerInterface } from 'src/app/interfaces/headerInterface';
 export class HeaderComponent {
 
   language: string = "";
-  header: headerInterface = {
-    home: '',
-    about: '',
-    skills: '',
-    contactMe: ''
-  }
 
   constructor(private _translationLoaderService: TranslationLoaderService, private _translateService: TranslateService) {
     this._translationLoaderService.loadTranslations(english, spanish);
     if(localStorage.getItem("lang")) {
-      this.language=localStorage.getItem("lang")!;
+      this._translateService.setDefaultLang(this.language);
     } else {
-      this.language='en';
+      this._translateService.setDefaultLang('en');
     }
-    // We set the translation data into the header variable
-    let translationData = this._translateService.translations[this.language];
-    this.header = translationData.header;
   }
 
   onlanguagechange($event: any) {
@@ -37,8 +28,5 @@ export class HeaderComponent {
     localStorage.setItem("lang", lang);
     this._translateService.setDefaultLang(lang);
     this._translateService.use(lang);
-    // We set the translation data into the header variable
-    let translationData = this._translateService.translations[this.language];
-    this.header = translationData.header;
   }
 }
