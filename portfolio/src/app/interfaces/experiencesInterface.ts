4a26a34dbebccd58327112a7abc0fa7cf6e15630
preytor <pgrfpgm@gmail.com>
2023-08-20 19:55:28 -0400

    ADded the interface for experiences

---
+++ portfolio/src/app/interfaces/experiencesInterface.ts
@@ -0,0 +1,9 @@
+export interface experiencesInterface {
+    title: string, 
+    company: string, 
+    period: string, 
+    technologies: string, 
+    missions: Array<string>, 
+    detailIsDisplayed: boolean,
+    image: string,
+}
