63a059a228a300063e93b89fe1c7b8d1be187219
preytor <pgrfpgm@gmail.com>
2023-08-30 18:09:30 -0400

    added dummy projects just to check how css behaves

--- portfolio/src/app/shared/projects/en.ts
+++ portfolio/src/app/shared/projects/en.ts
@@ -6,7 +6,130 @@ export const projectsEn = [
         images: [
             {
                 title: 'Portfolio',
-                src: "assets/images/projects/portfolio.png",
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
+            },
+        ],
+        url: 'https://github.com/preytor/portfolio',
+        detail: "<p> A responsive WEB site used to show my CV.</p>",
+        codeSourceUrl: 'https://github.com/preytor/portfolio',
+        detailIsDisplayed: false,
+    },
+    {
+        id: 'portfolio',
+        name: "Portfolio",
+        technologies: 'Angular16, Html5, CSS, TypeScript, BootStrap',
+        images: [
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
+            },
+        ],
+        url: 'https://github.com/preytor/portfolio',
+        detail: "<p> A responsive WEB site used to show my CV.</p>",
+        codeSourceUrl: 'https://github.com/preytor/portfolio',
+        detailIsDisplayed: false,
+    },
+    {
+        id: 'portfolio',
+        name: "Portfolio",
+        technologies: 'Angular16, Html5, CSS, TypeScript, BootStrap',
+        images: [
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
+            },
+        ],
+        url: 'https://github.com/preytor/portfolio',
+        detail: "<p> A responsive WEB site used to show my CV.</p>",
+        codeSourceUrl: 'https://github.com/preytor/portfolio',
+        detailIsDisplayed: false,
+    },
+    {
+        id: 'portfolio',
+        name: "Portfolio",
+        technologies: 'Angular16, Html5, CSS, TypeScript, BootStrap',
+        images: [
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
+            },
+        ],
+        url: 'https://github.com/preytor/portfolio',
+        detail: "<p> A responsive WEB site used to show my CV.</p>",
+        codeSourceUrl: 'https://github.com/preytor/portfolio',
+        detailIsDisplayed: false,
+    },
+    {
+        id: 'portfolio',
+        name: "Portfolio",
+        technologies: 'Angular16, Html5, CSS, TypeScript, BootStrap',
+        images: [
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
+            },
+        ],
+        url: 'https://github.com/preytor/portfolio',
+        detail: "<p> A responsive WEB site used to show my CV.</p>",
+        codeSourceUrl: 'https://github.com/preytor/portfolio',
+        detailIsDisplayed: false,
+    },
+    {
+        id: 'portfolio',
+        name: "Portfolio",
+        technologies: 'Angular16, Html5, CSS, TypeScript, BootStrap',
+        images: [
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv1.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv2.png",
+            },
+            {
+                title: 'Portfolio',
+                src: "assets/images/projects/cv/cv3.png",
             },
         ],
         url: 'https://github.com/preytor/portfolio',
